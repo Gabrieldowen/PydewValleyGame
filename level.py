e1920c216b2ea28a6e92bb62fbcb6d31720c12c1
Gabriel <Gabriel.d.owen-1@ou.edu>
2023-07-21 14:14:44 -0500

    Added Camera and basic logic

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from player import Player
 from settings import *
 from overlay import Overlay
+from sprites import Generic
 
 
 class Level:
@@ -18,10 +19,17 @@ class Level:
 
 	def setup(self):
 		self.player = Player((640, 360), self.all_sprites)
+		Generic(
+			pos=(0, 0),
+			surf=pygame.image.load('./graphics/world/ground.png'),
+			groups=self.all_sprites,
+			z=LAYERS['ground'])
 
 	def run(self, dt):
 		self.display_surface.fill('black')
-		self.all_sprites.draw(self.display_surface)
+		# self.all_sprites.draw(self.display_surface) not needed with custom_draw()
+		self.all_sprites.custom_draw(self.player)
+
 		self.all_sprites.update(dt)
 
 		self.overlay.display()
@@ -30,4 +38,17 @@ class Level:
 class CameraGroup(pygame.sprite.Group):
 	def __init__(self):
 		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.offset = pygame.math.Vector2()
 
+	def custom_draw(self, player):
+		self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
+
+
+		for layer in LAYERS.values():
+			for sprite in self.sprites():
+				if sprite.z == layer:
+					offset_rect = sprite.rect.copy()
+					offset_rect.center -= self.offset
+					self.display_surface.blit(sprite.image, offset_rect)
